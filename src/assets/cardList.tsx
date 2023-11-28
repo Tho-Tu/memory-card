112180d7dbebd7fd4d9a73a536b61ad2276a13e1
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-11-28 14:45:26 +1000

    Add list of pokemon

---
+++ src/assets/cardList.tsx
@@ -0,0 +1,37 @@
+type PokemonObject = {
+  id: number;
+  name: string;
+};
+
+export const pokemonList = [
+  { id: 0, name: "pikachu" },
+  { id: 1, name: "bulbasaur" },
+  { id: 2, name: "charmander" },
+  { id: 3, name: "squirtle" },
+  { id: 4, name: "caterpie" },
+  { id: 5, name: "totodile" },
+  { id: 7, name: "cyndaquil" },
+  { id: 8, name: "chikorita" },
+];
+
+export function shuffle(array: PokemonObject[]) {
+  let currentIndex = array.length,
+    randomIndex;
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+//  // Used like so
+//  var arr = [2, 11, 37, 42];
+//  shuffle(arr);
+//  console.log(arr);
